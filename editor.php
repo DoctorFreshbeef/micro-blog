bc175d32c148d18ecc89cc99e4fb2529e2e8289a
Dr. Freshbeef <rob@robzeeman.nl>
2022-04-13 21:56:53 +0200

    Full editor

--- editor.php
+++ editor.php
@@ -8,12 +8,19 @@ require('service/functions.php');
 <head>
     <title>Micro blog</title>
     <link rel="stylesheet" href="editor/pell.css">
+    <link rel="stylesheet" href="css/micro-editor.css">
     <script src="editor/pell.js"></script>
 </head>
 <body>
-<H2>Micro blog editor</H2>
-<div class="content">
+<div id="content">
+    <H2>Micro blog editor</H2>
+    <div id="editorForm">
+        <div class="formLabel">Head:</div>
+        <input type="text" id="blogHead" size="72">
+        <div class="formLabel">Body:</div>
     <div id="pell" class="pell" style="width: 600px"></div>
+        <input type="button" value="Save">
+    </div>
 </div>
 <script>
     var editor = window.pell.init({
